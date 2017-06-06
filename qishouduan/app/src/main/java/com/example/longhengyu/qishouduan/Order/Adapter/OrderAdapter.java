c9344e871d02e09c326c4380e6b9a608bc86d155
龙恒宇 <longhengyu@longhengyudeMac-mini.local>
2017-06-06 19:04:50 +0800

    增加订单列表item

--- qishouduan/app/src/main/java/com/example/longhengyu/qishouduan/Order/Adapter/OrderAdapter.java
+++ qishouduan/app/src/main/java/com/example/longhengyu/qishouduan/Order/Adapter/OrderAdapter.java
@@ -1,17 +1,32 @@
 package com.example.longhengyu.qishouduan.Order.Adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.longhengyu.qishouduan.R;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by yuanwuji on 2017/6/5.
  */
 
 public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> {
+
+
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_order_list, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
@@ -25,8 +40,21 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.text_orderList_item_orderNum)
+        TextView mTextOrderListItemOrderNum;
+        @BindView(R.id.text_orderList_item_address)
+        TextView mTextOrderListItemAddress;
+        @BindView(R.id.text_orderList_item_orderTime)
+        TextView mTextOrderListItemOrderTime;
+        @BindView(R.id.text_orderList_item_footTime)
+        TextView mTextOrderListItemFootTime;
+        @BindView(R.id.text_orderList_item_price)
+        TextView mTextOrderListItemPrice;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
---
+++ qishouduan/app/src/main/java/com/example/longhengyu/qishouduan/Order/Bean/OrderListBean.java
@@ -0,0 +1,8 @@
+package com.example.longhengyu.qishouduan.Order.Bean;
+
+/**
+ * Created by longhengyu on 2017/6/6.
+ */
+
+public class OrderListBean {
+}
